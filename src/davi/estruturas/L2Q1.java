db33ecdf62ed28885ddf7bfae650355e949b239e
Davi <davivinici71@gmail.com>
2026-05-01 21:00:29 -0300

    feat: L2Q1

--- src/davi/estruturas/L2Q1.java
+++ src/davi/estruturas/L2Q1.java
@@ -2,21 +2,18 @@ package davi.estruturas;
 
 import java.util.Scanner;
 
-public class Main {
+public class L2Q1 {
     static int m; // definimos o tamanho da lista
     static int[] lista; // lista
     static int topo = -1; // topo da lista
 
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Tamanho M inicial tem que ser definido pelo usuário
         System.out.println("Defina a capacidade máxima da lista (M)");
         m = sc.nextInt();
         lista = new int[m];
-
-
-        // Mensagem para o usuário
         int sair = 0;
         int x;
         while(sair != 1){
@@ -36,7 +33,7 @@ public class Main {
                 case 2:
                     System.out.println("Digite o número que deseja remover: ");
                     x = sc.nextInt();
-                    remocao();
+                    remocao(x);
                     break;
                 case 3:
                     System.out.println("Digite o número que deseja buscar: ");
@@ -61,66 +58,65 @@ public class Main {
     }
     public static void impressao(){
         System.out.print("[");
-         for(int i = 0; i <= m; i++){
+         for(int i = 0; i < m; i++){
              if(i <= topo){
-                 System.out.print(lista[i] + " ");
+                 System.out.print(lista[i] + ", ");
              }
              else {
-                 System.out.print(" NIL");
+                 System.out.print(" NIL, ");
              }
         }
         System.out.println("]");
     }
 
     public static int busca(int x){ // X é o valor que quer ser buscado e M é o tamanho da lista
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i <= topo; i++){
             if(lista[i] == x){
-                return lista[i];
+                System.out.println("O numero "+ x + " foi encontrado na lista.");
+                return i;
             }
         }
-        throw new IllegalArgumentException("O item que você buscou não está na lista!");
+        System.out.println("O numero " + x + " Não foi encontrado na lista.");
+        return -1;
     }
 
     public static void insercao(int x){ // X é o valor a ser inserido e M é o tamanho da lista
-        topo = topo + 1;
-        if(topo == m) {
+        if(topo + 1 == m) {
             int[] lista2 = new int[m * 2];
-            for(int i = 0; i < m; i++){
+
+            for(int i = 0; i <= topo; i++){
                 lista2[i] = lista[i];
             }
-            lista2[m + 1] = x;
+            lista = lista2;
             m = m * 2;
+            System.out.println("A lista chegou no seu tamanho máximo, o tamanho foi aumentado para : " + m);
+        }
+        int j = topo;
+        while(j >= 0 && lista[j] > x) {
+            lista[j + 1] = lista[j];
+            j = j - 1;
+        }
+        lista[j + 1] = x;
+        topo = topo + 1;
+        System.out.println("O valor: " + x + " Foi inserido!");
+    }
+    public static int remocao(int x){
+        int aux = busca(x);
+        if(aux == -1){
+            System.out.println("Número não encontrado na lista.");
+            return -1;
         }
         else{
-            lista[topo] = x;
-        }
-        insertion_sort();
-    }
-    public static int remocao(){
-        if(topo == -1){
-            throw new IllegalArgumentException("Não é possível remover um item de uma lista vazia");
-        }
-        int aux = lista[topo];
-        topo = topo - 1;
-        lista[topo + 1] = 0;
-        System.out.println("Valor" + aux + "removido.");
-        return aux;
-    }
-
-    public static void insertion_sort(){
-        if(topo > 0){
-            for (int j = 1; j < topo; j++){
-                int i = j - 1;
-                int chave = lista[j];
-                while(i >= 0 && chave < lista[i]){
-                    lista[i + 1] = lista[i];
-                    i = i - 1;
-                }
-                lista[i + 1] = chave;
+            for(int i = aux; i < topo; i++){
+                lista[i] = lista[i + 1];
             }
+            topo--;
+            System.out.println("O valor: " + x + " Foi removido!");
+            return aux;
         }
 
     }
 
 
-}
+    }
+
---
+++ src/davi/estruturas/L2Q2.java
@@ -0,0 +1,25 @@
+package davi.estruturas;
+
+import java.util.Scanner;
+
+public class L2Q2 {
+    static int m; // definimos o tamanho da lista
+    static int[] lista; // lista
+     int inicio = 1; // topo da lista
+    static int fim = 2;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Defina a capacidade máxima da lista (M)");
+        m = sc.nextInt();
+        lista = new int[m];
+
+
+    }
+    public void mergeSort(){
+        if(fim - inicio > 1){
+
+        }
+    }
+
+
+}
